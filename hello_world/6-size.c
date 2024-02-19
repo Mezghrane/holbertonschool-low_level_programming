3ffb87d5283afa295786038575ce634fbb4158c0
mezghrane <8802@holbertonstudents.com>
2024-02-19 05:01:34 -0800

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+       	printf("Size of char: %lu\nSize of int: %lu\nSize of long int: %lu\nSize of long long int: %lu\nSize of float: %lu\nSize of double: %lu\nSize of long double: %lu\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float), sizeof(double), sizeof(long double));
+	return (0);
+}
