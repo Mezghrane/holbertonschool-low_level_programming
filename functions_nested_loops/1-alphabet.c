dba1df5ae1fc2bc526a6f3d927238be0f8bc1b7c
mezghrane <8802@holbertonstudents.com>
2024-02-20 05:33:20 -0800

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void);
 {
 char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
+	putchar(letter);
 putchar('\n');
 
-        return (0);
+return (0);
 }
