a172f93524bc3bafd0eeb9c9514cf67d9ffb928e
mezghrane <8802@holbertonstudents.com>
2024-02-20 01:49:59 -0800

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,12 +10,12 @@ int main(void)
 	int number;
 
 	for (number = '0'; number <= '9'; number++)
-		.putchar(number);
+		putchar(number);
 	if (number != '9')
 
 	{
 		putchar(',');
-		putchar(' ')
+		putchar(' ');
 	}
 
 	putchar('\n');
