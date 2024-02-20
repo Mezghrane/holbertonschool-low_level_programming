42f92f6d25b3a05408d0e579b18a3259eb795f2d
mezghrane <8802@holbertonstudents.com>
2024-02-20 03:18:10 -0800

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,11 +6,17 @@
  * Return: Always 0
  */
 int main(void)
-int _putchar(char c);
+
 {
-    return write(1, &c, 1);
-}
-{
-	_putchar("_putchar\n");
-	return (0);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+    
+    return (0);
 }
