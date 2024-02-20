40975cea7e3a5a1cb3c150adaaf9587477a76526
mezghrane <8802@holbertonstudents.com>
2024-02-20 13:15:42 -0800

    end

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,6 @@
  * Return: Always 0.
  */
 
-void _putchar(char c)
-{
-    putchar(c);
-}
-
 void print_alphabet_x10(void)
 {
 	int i;
--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+int _putchar(char c)
 {
     return putchar(c);
 }
