ab60cc87729d5b7f8486c5a01f7a91a9293afbae
mezghrane <8802@holbertonstudents.com>
2024-02-20 12:03:37 -0800

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,12 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
 int _putchar(char c);
+void print_alphabet_x10(void);
+
+#endif
