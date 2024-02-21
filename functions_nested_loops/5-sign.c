18d0f817d5a68dcac42f6e22cd34f08704c45f62
mezghrane <8802@holbertonstudents.com>
2024-02-21 05:24:18 -0800

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,20 +8,21 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c);
 
 /**
  * print_sign - checks the sign of a number and prints it
  * @n: the number to check
  *
- * This function checks if the input number `n` is positive, zero, or negative.
- * It prints the corresponding sign and returns the corresponding value.
+ * Description: This function checks if the input number `n` is positive,
+ * zero, or negative. It prints the corresponding sign and returns the
+ * corresponding value.
  *
  * Return: 1 and prints '+' if n is greater than zero
  *         0 and prints '0' if n is zero
  *         -1 and prints '-' if n is less than zero
  */
 
-int _putchar(char c);
 
 int print_sign(int n)
 {
