86632682fbb03c838f1553d69c6959e626a7e044
mezghrane <8802@holbertonstudents.com>
2024-02-21 04:54:07 -0800

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,16 +8,15 @@
  * Return: 1 if c is a letter, lowercase or uppercase
  *         0 otherwise
  */
+int _isalpha(int c)
 
-int _isalpha(int c);
 {
-
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
-	return (1);
-}
-else
-{
-	return (0);
-}
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
