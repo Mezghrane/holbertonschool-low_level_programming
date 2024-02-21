96cbe5db73a86ccb738015ad4fdf3cba8109a7f2
mezghrane <8802@holbertonstudents.com>
2024-02-21 06:30:52 -0800

    _

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,13 +10,23 @@
  * formatted with leading zeros if necessary.
  */
 
+void _putchar(char c);
+
 void jack_bauer(void)
 {
-	for (int hour = 0; hour < 24; hour++)
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (int minute = 0; minute < 60; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{
-			printf("%02d:%02d\n", hour, minute);
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
 		}
 	}
 }
+
