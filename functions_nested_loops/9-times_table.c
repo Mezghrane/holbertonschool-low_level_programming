fcbc3bd32aa7c4f5f7d19c297e5392f58ee607d2
mezghrane <8802@holbertonstudents.com>
2024-02-22 19:58:43 +0000

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,24 +15,21 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			if (j < 9)
-
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-			if (product < 10)
-			{
-				_putchar(' ');
-				_putchar(product + '0');
-			} else
+			if (product > 9)
 			{
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
+			} else
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
 	}
 }
-
