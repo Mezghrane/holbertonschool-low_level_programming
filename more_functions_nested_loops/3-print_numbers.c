25964ea75e70aab9cf7dc5c2ae77a32aac8281f5
mezghrane <8802@holbertonstudents.com>
2024-02-26 19:14:13 +0000

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,8 +3,25 @@
 #include <unistd.h>
 
 /**
+ * _putchar - pour imprimer un chharactere a la fois 
+ * @c: character to be output
+ */
+void _putchar(char c)
+{
+
+/**
+ * write- c'est pour ecrire 
+ *
+ *
+ */
+
+    write(1, &c, 1);
+}
+
+
+/**
  * print_numbers - imprime les chiffres de 0 a 9
- * _putchar - affiche 1 charactere a la fois 
+ * 
  */
 
 void print_numbers(void)
