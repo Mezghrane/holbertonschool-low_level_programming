6831abffb195a16c675b4b7e08404fe3c1ad287e
mezghrane <8802@holbertonstudents.com>
2024-02-26 20:08:33 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+
+
+
+
+void more_numbers(void)
+{
+	int i,j
+		for (i = 0; i < 10; i++)
+		{	 for (j = 1; j< 14; j++)
+			_putchar(j);
+		}
+	_putchar('\n')
+}
