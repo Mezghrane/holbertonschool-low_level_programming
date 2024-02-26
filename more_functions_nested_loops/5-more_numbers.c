b970fd175434e38872e0d1a29f3cf4a2b297f429
mezghrane <8802@holbertonstudents.com>
2024-02-26 20:43:01 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,20 @@ void more_numbers(void)
 {
 	int i,j;
 	for (i = 0; i < 10; i++)
-	{	 for (j = 1; j <= 14; j++)
+	{
+
+		for (j = 1; j <= 14; j++)
 		{
-			if (j > 9)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
-			_putchar('\n');
+			{
+				if (j > 9)
+					_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+
+			}
+			{
+				if (j == 14)
+					_putchar('\n');
+			}
 		}
 	}
 }
