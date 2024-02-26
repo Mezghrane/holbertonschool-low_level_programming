69013f272cacfd7a4ee2a978fc43453ea6d73dbb
mezghrane <8802@holbertonstudents.com>
2024-02-26 15:48:29 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ */
+int _putchar(char c); 
+
+/**
+ * print_numbers - imprime les chiffres de 0 a 9
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
+
