87e1773b3fef5f38324675168bf104066419a80b
mezghrane <8802@holbertonstudents.com>
2024-02-27 12:04:53 +0000

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,17 +13,19 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for(i = 1; i < n; i++)
-	{	
+	for (i = 1; i < n; i++)
+	{
 		_putchar ('\\');
 
 	}
-	 _putchar ('\n');
+	_putchar ('\n');
 
-	for(j = 0; j < n; j++)
+	for (j = 0; j < n; j++)
 
 
-	{ 
+	{
 		_putchar (' ');
 	}
+
+	_putchar ('\n');
 }
