2782ada34397fe90b68f6ceaca6fdf92ebddd911
mezghrane <8802@holbertonstudents.com>
2024-02-28 13:28:07 +0000

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,13 +3,10 @@
 
 
 /**
- * @print_triangle
- * Prints a right-angled triangle composed of '#' characters.
- *
- * The size of the triangle is determined by the parameter 'size'. If 'size' is
- * less than or equal to 0, the function does not print anything.
- *
+ * print_triangle - Prints a right-angled triangle composed of '#' characters.
  * @size The size of the triangle.
+ *
+ * Return : int size
  */
 
 void print_triangle(int size)
@@ -30,7 +27,7 @@ void print_triangle(int size)
 		}
 	}
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < j; i++)
 	{
 		_putchar('#');
 		_putchar('\n');
