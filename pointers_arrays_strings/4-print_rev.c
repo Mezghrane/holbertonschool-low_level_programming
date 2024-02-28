2cddaa2b3b43193e9007c96852beb99c88f96883
mezghrane <8802@holbertonstudents.com>
2024-02-28 18:37:37 +0000

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,19 +11,19 @@
 
 void print_rev(char *s)
 {
-	int i; 
+	int i;
 
-        int length = 0;
-    
-    while (s[length] != '\0') 
-    {
-        length++;
-    }
-    
-    for (i = length - 1; i >= 0; i--) 
-    {
-        write(1, &s[i], 1);
-    }
+	int length = 0;
 
-    write(1, "\n", 1);
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		write(1, &s[i], 1);
+	}
+
+	write(1, "\n", 1);
 }
