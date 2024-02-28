e8f5079e3a64453bc4f4ff2fb3bc97da16c87d2e
mezghrane <8802@holbertonstudents.com>
2024-02-28 08:45:12 +0000

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,6 +18,7 @@ int main (void)
 
 	for (i = 1; i <= 100; i++)
 	{
+	{
 		if (i % 3 == 0 && i % 5 != 0) 
 			printf("Fizz");
 		else if (i % 5 == 0 && i % 3 != 0)
@@ -27,13 +28,19 @@ int main (void)
 		else    printf("%d", i);
 	}
 
-	{
-	       	if (i < 100)
-			printf(" ");
 		
-		else	printf("\n");
+	       	if (i < 100)
+		
+		{		
+			printf(" ");
+		}
+		
+		else	
+		{
+			printf("\n");
+		}
+		
 	}
-	
 	return (0);
 
 }
